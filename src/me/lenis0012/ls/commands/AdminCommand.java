0eb9549475a384882812759fe43661514608d1b1
Lennart <0p1q9o2w@hotmail.nl>
2012-12-05 08:32:09 +0100

    v1.6.7

--- src/me/lenis0012/ls/commands/AdminCommand.java
+++ src/me/lenis0012/ls/commands/AdminCommand.java
@@ -1,5 +1,9 @@
 package me.lenis0012.ls.commands;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import me.lenis0012.ls.LoginData;
 import me.lenis0012.ls.ls;
 
@@ -30,6 +34,7 @@ public class AdminCommand implements CommandExecutor
 		if(!player.hasPermission("ls.admin"))
 		{
 			player.sendMessage(r+"No permission");
+			return true;
 		}
 		
 		if(args.length == 0)
@@ -43,6 +48,14 @@ public class AdminCommand implements CommandExecutor
 		else if(args[0].equalsIgnoreCase("changepass"))
 		{
 			this.changePass(player, args);
+		}
+		else if(args[0].equalsIgnoreCase("setpass"))
+		{
+			this.setPass(player, args);
+		}
+		else if(args[0].equalsIgnoreCase("rmpass"))
+		{
+			this.rmPass(player, args);
 		}
 		else
 		{
@@ -60,8 +73,10 @@ public class AdminCommand implements CommandExecutor
 	public void showInfo(Player player)
 	{
 		player.sendMessage(i+"==========-{ "+r+"L"+g+"oginSecurity "+r+"A"+g+"dmin "+r+"C"+g+"ommand "+i+"}-==========");
-		player.sendMessage(g+"/lac changePass (Player) (Password)"+i+" - change a players pass");
-		player.sendMessage(g+"/lac changePass reload"+i+" - ReloadConfig");
+		player.sendMessage(g+"/lac changePass (Player) (Password)"+i+" - Change a players pass");
+		player.sendMessage(g+"/lac setPass (Player) (Password)"+i+" - Set a players password");
+		player.sendMessage(g+"/lac rmPass (Player)"+i+" - Remove a players password");
+		player.sendMessage(g+"/lac reload"+i+" - Reload the config");
 	}
 	
 	public void reload(Player player)
@@ -79,7 +94,46 @@ public class AdminCommand implements CommandExecutor
 			player.sendMessage(ChatColor.RED + "Invalid args");
 			return;
 		}
-		LoginData.setPass(args[1], args[2], plugin, 1);
-		player.sendMessage(g+"Changed password from "+args[1]+" to "+args[2]);
+		String pass = args[2];
+		if(plugin.getConfig().getBoolean("options.use-MD5 Enryption"))
+			pass = md5(pass);
+		LoginData.setPass(args[1], pass, plugin, 1);
+		player.sendMessage(g+"Changed password for "+args[1]+" to "+args[2]);
+	}
+	
+	public void setPass(Player player, String[] args)
+	{
+		if(args.length < 3)
+		{
+			player.sendMessage(ChatColor.RED + "Invalid args");
+			return;
+		}
+		String pass = args[2];
+		if(plugin.getConfig().getBoolean("options.use-MD5 Enryption"))
+			pass = md5(pass);
+		LoginData.setPass(args[1], pass, plugin, 0);
+		player.sendMessage(g+"Set password for "+args[1]+" to "+args[2]);
+	}
+	
+	public void rmPass(Player player, String[] args)
+	{
+		if(args.length < 3)
+		{
+			player.sendMessage(ChatColor.RED + "Invalid args");
+			return;
+		}
+		LoginData.delPass(args[1], plugin);
+		player.sendMessage(g+"Removed password from "+args[1]);
+	}
+	
+	public String md5(String str)
+	{
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("MD5");
+			md.update(str.getBytes(), 0, str.length());
+			return new BigInteger(1, md.digest()).toString(16);
+		} catch (NoSuchAlgorithmException e) {}
+		return "";
 	}
 }
